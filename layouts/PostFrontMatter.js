3c1ecc8addb58507d9644c3db0f6469d81d4b74e
sudokpr <sudokpr@gmail.com>
2022-02-06 09:14:25 +0530

    Added the new layout file missed in prev commit
    #6

---
+++ layouts/PostFrontMatter.js
@@ -0,0 +1,89 @@
+import Link from '@/components/Link'
+import Image from '@/components/Image'
+import PageTitle from '@/components/PageTitle'
+import SectionContainer from '@/components/SectionContainer'
+import { BlogSEO } from '@/components/SEO'
+import siteMetadata from '@/data/siteMetadata'
+import formatDate from '@/lib/utils/formatDate'
+import Comments from '@/components/comments'
+import ScrollTopAndComment from '@/components/ScrollTopAndComment'
+
+export default function PostLayout({ frontMatter, authorDetails, next, prev, children }) {
+  const { date, title, subrecipe = [] } = frontMatter
+
+  return (
+    <SectionContainer>
+      <BlogSEO url={`${siteMetadata.siteUrl}/recipe/${frontMatter.slug}`} {...frontMatter} />
+      <ScrollTopAndComment />
+      <article>
+        <div>
+          <header>
+            <div className="pb-10 space-y-1 text-center border-b border-gray-200 dark:border-gray-700">
+              <dl>
+                <div>
+                  <dt className="sr-only">Published on</dt>
+                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                    <time dateTime={date}>{formatDate(date)}</time>
+                  </dd>
+                </div>
+              </dl>
+              <div>
+                <PageTitle>{title}</PageTitle>
+              </div>
+            </div>
+          </header>
+          <div
+            className="pb-8 divide-y divide-gray-200 xl:divide-y-0 dark:divide-gray-700 "
+            style={{ gridTemplateRows: 'auto 1fr' }}
+          >
+            <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:pb-0 xl:col-span-3 xl:row-span-2">
+              <div className="pt-10 pb-8 prose dark:prose-dark max-w-none">
+                {subrecipe.map((section, index) => (
+                  <div key={index}>
+                    <h2> {section.name} </h2>
+                    <h3> Ingredients </h3>
+                    <div className="whitespace-pre">{section.ingredients}</div>
+                    <h3> Instructions </h3>
+                    <div className="whitespace-pre">{section.instructions}</div>
+                    {section.story.map((step, index) => (
+                      <div key={index}>
+                        <h3> {step.title} </h3>
+                        <h4> {step.description} </h4>
+                        <Image width="900" height="900" layout="responsive" src={step.image} />
+                      </div>
+                    ))}
+                  </div>
+                ))}
+              </div>
+            </div>
+            <Comments frontMatter={frontMatter} />
+            <footer>
+              <div className="flex flex-col text-sm font-medium sm:flex-row sm:justify-between sm:text-base">
+                {prev && (
+                  <div className="pt-4 xl:pt-8">
+                    <Link
+                      href={`/recipe/${prev.slug}`}
+                      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                    >
+                      &larr; {prev.title}
+                    </Link>
+                  </div>
+                )}
+                {next && (
+                  <div className="pt-4 xl:pt-8">
+                    <Link
+                      href={`/recipe/${next.slug}`}
+                      className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                    >
+                      {next.title} &rarr;
+                    </Link>
+                  </div>
+                )}
+              </div>
+            </footer>
+          </div>
+        </div>
+      </article>
+    </SectionContainer>
+  )
+}
